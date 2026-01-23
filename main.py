eca21ee611fa5d43b0c09a2231183cb633f02795
Hurtubise-David <hurtubisedavid@gmail.com>
2026-01-22 21:29:38 -0500

    adjust missing mono action

--- main.py
+++ main.py
@@ -2661,6 +2661,8 @@ class AppWindow(QtWidgets.QMainWindow):
         # Calibrate
         m_cal = mb.addMenu("Calibrate")
         act_cmono = QtWidgets.QAction("Calibrate Mono", self)
+        m_cal.addAction(act_cmono)
+        act_cmono.triggered.connect(lambda: self.stack.setCurrentWidget(self.page_calib_mono))
         # Pose Estimation
         m_pose = mb.addMenu("Pose Estimation")
         act_live = QtWidgets.QAction("Live Pose", self)
